3eabe5419f2f3379ab48977b74469982723fad1c
Sandeep Choudhary <76850569+sandeepamarchoudhary@users.noreply.github.com>
2024-03-18 12:51:41 +0530

    remove repeated line

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,10 +50,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json()
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
-
 
 
 
